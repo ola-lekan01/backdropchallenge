f79aa8abe5b42f1aa4595e65b70e28955ada4568
Ola Lekan <lekan.sofuyi01@gmail.com>
2023-04-10 08:13:39 +0100

    BACKDROP: Fixes to Test Case

--- src/test/java/com/backdrop/service/impl/PayStackServiceImplTest.java
+++ src/test/java/com/backdrop/service/impl/PayStackServiceImplTest.java
@@ -12,11 +12,15 @@ class PayStackServiceImplTest {
     // Kindly pass in the Paystack secret key in the class implementation
     @Test
     void testThatNameFetchedByAPIIsReturnedIfUserDoesntProvideAnAccountName() throws IOException {
+        // Setting up Variables
         String accountName = "Sofuyi Olalekan Samson";
         String accountNumber = "0108510992";
         String bankCode = "058";
 
+        // Making a method call
         Data accountData = payStackService.getAccountName(accountNumber, bankCode);
+
+        // Asserting Responses
         assertEquals(accountName.toUpperCase(), accountData.getAccount_name());
     }
 }
